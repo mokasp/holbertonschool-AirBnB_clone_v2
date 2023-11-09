cd8d0e7e4b955dc072dac1d00ece938c6be0854f
mokasp <kasperbouldin@gmail.com>
2023-11-09 16:23:37 -0600

    11-9-23 | 4:26pm added web app that will decide if number is odd or even and reflect that with HTML template

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" module containing 7 simple web applications with flask """
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c(text):
+    new_text = text.replace("_", " ")
+    return f'C {escape(new_text)}'
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python(text='is cool'):
+    new_text = text.replace("_", " ")
+    return f'Python {escape(new_text)}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f'{escape(n)} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_templates(n):
+    return render_template("5-number.html", n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    if (n % 2) == 0:
+        even_odd = "even"
+    else:
+        even_odd = "odd"
+    return render_template("6-number_odd_or_even.html", n=n, even_odd=even_odd)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
