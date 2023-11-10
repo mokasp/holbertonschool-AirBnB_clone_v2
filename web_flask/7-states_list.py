a17afe0bdb2e0ebd57697b75040c5b3749611fa8
mokasp <kasperbouldin@gmail.com>
2023-11-10 13:08:13 -0600

    11-10-23 | 1:08pm | web app that uses template to list all state objects

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" module containing 7 simple web applications with flask """
+from flask import Flask, render_template, g
+from models import storage
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    from models.state import State
+    states = []
+    x = storage.all(State)
+    for item in x:
+        states.append(x[item])
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
