20d21fa43ffd31bce37e1e99e018c8884214b051
mokasp <kasperbouldin@gmail.com>
2023-11-10 12:59:03 -0600

    11-10-23 | 1:00pm | add template and web app that loops through lists and displays name and id of objects

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" module containing 7 simple web applications with flask """
+from flask import Flask, render_template, g
+from models import storage
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_state():
+    from models.state import State
+    states = []
+    x = storage.all(State)
+    for item in x:
+        states.append(x[item])
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
